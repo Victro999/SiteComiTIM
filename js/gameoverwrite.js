17c2d8359c293c395a85b139d2ec2a4cdcf85f53
Victro <victoraubry999@gmail.com>
2023-10-25 07:18:49 -0400

    GameFormatUpdate

--- js/gameoverwrite.js
+++
@@ -1,8 +0,0 @@
-var canvas = document.getElementById("unity-canvas");
-canvas.style.borderRadius = "1em";
-
-var style = document.head.appendChild(document.createElement("style"));
-style.innerHTML = "@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@100;400;600&display=swap')";
-var title = document.getElementById("unity-build-title");
-title.style.fontFamily = "\"Kanit\", sans-serif";
-title.style.fontSize = "2em";
