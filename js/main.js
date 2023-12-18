08123c2ae672dc2d3ebd55b6f863f6e54bdf7752
Victro <victoraubry999@gmail.com>
2023-12-18 14:40:52 -0500

    Preparing new nav

--- js/main.js
+++ js/main.js
@@ -7,18 +7,6 @@ function screenWidth(){
     return isMobile()? screen.width : window.innerWidth;
 }
 
-window.addEventListener("resize",debounce(function(e){
-    switchPage(currentPageID);
-}));
-
-function debounce(func){
-    var timer;
-    return function(event){
-      if(timer) clearTimeout(timer);
-      timer = setTimeout(func,100,event);
-    };
-}
-
 //---------- Float ----------//
 var floatingElements = document.getElementsByClassName("floating");
 var floatingTimer = 0;
@@ -31,28 +19,6 @@ setInterval(() => {
 }, 5);
 
 //---------- Navigation ----------//
-var main = document.getElementsByTagName("main")[0];
-const navBtns = document.querySelectorAll("nav button");
-var listeProjets = document.getElementById("liste-projets");
-var listeActivites = document.getElementById("liste-activites");
-var currentPageID;
-function switchPage(ID)
-{
-    currentPageID = ID;
-    main.style.right = ID * screenWidth() + "px";
-    for (var i = 0; i < navBtns.length; i++)
-    {
-        if (i + 1 === ID) navBtns[i].classList.add("currentPage");
-        else navBtns[i].classList.remove("currentPage");
-    }
-
-    if (ID === 1) listeProjets.classList.remove("hide");
-    else listeProjets.classList.add("hide");
-
-    if (ID === 2) listeActivites.classList.remove("hide");
-    else listeActivites.classList.add("hide");
-}
-
 var contentProjets = document.querySelectorAll("#projets > div");
 var contentActivites = document.querySelectorAll("#activites > div");
 var btnsProjets = document.querySelectorAll("#liste-projets button");
@@ -94,14 +60,14 @@ function showHeader()
 }
 
 //---------- Games ----------//
-function gameBtn(name)
+function gameBtn(gameName)
 {
-    var iframe = document.querySelector(`#${name} iframe`);
+    var iframe = document.querySelector(`#${gameName} iframe`);
     var img = iframe.parentElement.getElementsByTagName("img")[0];
-    var span = document.querySelector(`#${name} span`);
+    var span = document.querySelector(`#${gameName} span`);
     if (span.innerHTML === "play_arrow")
     {
-        iframe.src = `games/${name}/${name}.html`;
+        iframe.src = `games/${gameName}/${gameName}.html`;
         img.style.display = "none";
         span.innerHTML = "pause";
     }
@@ -111,20 +77,4 @@ function gameBtn(name)
         img.style.display = "block";
         span.innerHTML = "play_arrow";
     }
-}
-
-function loadGame(name)
-{
-    var iframe = document.querySelector(`#${name} iframe`);
-    iframe.src = `games/${name}/${name}.html`;
-    iframe.parentElement.getElementsByTagName("img")[0].style.display = "none";
-    document.querySelector(`#${name} span`).innerHTML = "pause";
-}
-
-function unloadGame(name)
-{
-    var iframe = document.querySelector(`#${name} iframe`);
-    iframe.src = "";
-    iframe.parentElement.getElementsByTagName("img")[0].style.display = "block";
-    document.querySelector(`#${name} span`).innerHTML = "play_arrow";
 }
